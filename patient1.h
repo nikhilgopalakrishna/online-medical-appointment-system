b7ddf6fb8a83366008a17877c08db290e6de284c
Nikhil <nikhil.kishore.g@gmail.com>
2015-09-14 16:36:58 -0800

    Create patient1.h

---
+++ patient1.h
@@ -0,0 +1,11 @@
+typedef struct patient_info{
+    char username[30];
+    char password[30];
+    char insurance[30];
+}patientinfo_t;
+
+typedef struct doctor_info{
+    char doctor[10];
+    char port[10];
+}doctorinfo_t;
+
